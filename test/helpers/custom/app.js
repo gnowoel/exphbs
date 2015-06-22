20235001e82a967fb194be0d39d2846d40b21420
Leo Wong <gnowoel@gmail.com>
2015-06-22 16:27:20 +0800

    Update test cases for helpers

--- test/helpers/custom/app.js
+++ test/helpers/custom/app.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var express = require('express');
+var exphbs = require('../../..');
 var app = express();
 
-app.engine('hbs', require('../..').create());
+app.engine('hbs', exphbs.create());
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.set('view helpers', path.join(__dirname, 'helpers'))
 
 app.get('/', function(req, res) {
   res.render('index', {
--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,26 +1,2 @@
-var assert = require('chai').assert;
-var request = require('request');
-var app = require('./app');
-
-describe('helpers', function() {
-
-  before(function(done) {
-    server = app.listen(3000, function() {
-      done();
-    });
-  });
-
-  it('can register Handlebars helpers', function(done) {
-    request('http://localhost:3000/', function(err, res, body) {
-      assert.include(body, '<a href="http://www.google.com/">Google</a>');
-      done();
-    });
-  });
-
-  after(function(done) {
-    server.close(function() {
-      done();
-    });
-  });
-
-});
+require('./autoload');
+require('./custom');
